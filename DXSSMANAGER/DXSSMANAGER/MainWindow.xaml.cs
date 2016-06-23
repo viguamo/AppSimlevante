3d472cbbe84f27b5b72c533f2cda30dbcc6b49af
vicente <viguamo@gmail.com>
2016-06-23 13:36:02 +0200

    añadido msgbox JL

--- DXSSMANAGER/DXSSMANAGER/MainWindow.xaml.cs
+++ DXSSMANAGER/DXSSMANAGER/MainWindow.xaml.cs
@@ -30,6 +30,7 @@ namespace DXSSMANAGER
             MessageBox.Show("Hola nacho");
             MessageBox.Show("Hola vicente");
             MessageBox.Show("Hola JV");
+            MessageBox.Show("Hola JL");
         }
     }
 }
